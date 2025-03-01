4625bb398e85f8999d16f3d55c2e780722ff0f95
radicalsaturday29 <167376854+radicalsaturday29@users.noreply.github.com>
2025-03-01 02:47:55 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const {Client, Events, SlashCommandBuilder } = require("discord.js");
+const {token} = require("./config.json");
+
+const client = new Client({intents: []});
+
+client.once(Events.ClientReady, c => {
+console.log(`Logged in as ${c.user.username}`);
+
+const ping = new SlashCommandBuilder()
+.setName("ping")
+.setDescription("Replies with pong!");
+
+client.application.commands.create(ping);
+});
+
+client.on(Events.InteractionCreate, interaction => {
+if(interaction.commandName === "ping"){
+   interaction.reply("Pong!");
+}  
+});
+
+client.login(token);
